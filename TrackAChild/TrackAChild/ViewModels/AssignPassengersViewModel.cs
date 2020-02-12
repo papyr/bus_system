f0a31308443cc6a7c4f70140b00296861201640c
Suzan Aydin <suzan_aydin7@hotmail.co.uk>
2020-02-12 21:54:23 +0000

    added filtering to passenger listview

--- TrackAChild/TrackAChild/ViewModels/AssignPassengersViewModel.cs
+++ TrackAChild/TrackAChild/ViewModels/AssignPassengersViewModel.cs
@@ -6,6 +6,7 @@ using TrackAChild.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System.Collections.Generic;
 using Windows.UI.Xaml.Controls;
+using System.Linq;
 
 namespace TrackAChild.ViewModels
 {
@@ -20,6 +21,28 @@ namespace TrackAChild.ViewModels
         public ObservableCollection<PassengerModel> Passengers
         {
             get { return passengerService.Passengers; }
+        }
+
+        private ObservableCollection<PassengerModel> filteredPassengers;
+        public ObservableCollection<PassengerModel> FilteredPassengers
+        {
+            get { return filteredPassengers; }
+            set { filteredPassengers = value; OnPropertyChanged(nameof(FilteredPassengers)); }
+        }
+
+        private string typeSelected;
+        public string TypeSelected
+        {
+            get { return typeSelected; }
+            set
+            {
+                if (typeSelected != value)
+                {
+                    typeSelected = value;
+                    OnPropertyChanged(nameof(TypeSelected));
+                    FilterPassengersToDisplay();
+                }
+            }
         }
 
         // A list to store the current selected passengers
@@ -50,6 +73,9 @@ namespace TrackAChild.ViewModels
             routeService = (App.Current as App).Container.GetService<IRouteService>();
             passengerService = (App.Current as App).Container.GetService<IPassengerService>();
 
+            // Set type
+            TypeSelected = "All";
+
             AssignPassengersCommand = new RelayCommand<object>((param) =>
             {
                 routeService.AssignPassengersToRoute(selectedPassengers);
@@ -67,6 +93,24 @@ namespace TrackAChild.ViewModels
             foreach (PassengerModel unSelectedItem in e.RemovedItems)
             {
                 selectedPassengers.Remove(unSelectedItem);
+            }
+        }
+
+        private void FilterPassengersToDisplay()
+        {
+            if (TypeSelected == "Student")
+            {
+                FilteredPassengers = new ObservableCollection<PassengerModel>(
+                    Passengers.Where(x => x.GetType() == typeof(StudentModel)).ToList());
+            }
+            else if (TypeSelected == "Chaperone")
+            {
+                FilteredPassengers = new ObservableCollection<PassengerModel>(
+                    Passengers.Where(x => x.GetType() == typeof(ChaperoneModel)).ToList());
+            }
+            else
+            {
+                FilteredPassengers = new ObservableCollection<PassengerModel>(Passengers);
             }
         }
     }
